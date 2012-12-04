011fe522519192f680522e3c2e402f96d9905a23
hylozero <hylozero@gmail.com>
2012-12-04 17:22:53 -0200

    Ajustes de rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Integra::Application.routes.draw do
-  mount IntegraNews::Engine, :at => "/novidades"
-
-  mount SelecaoAdmin::Engine, :at => "/selecao-admin"
-
   get "enrolled_data_table/index"
   
   get "settings/index"
